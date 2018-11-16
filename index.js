0956b3ee99cd1ae4ae45b4f1a8a632ec383e3aa2
Robin Berjon <robin@berjon.com>
2018-11-15 23:33:31 -0500

    bring in SSE signaling

--- index.js
+++ index.js
@@ -11,16 +11,19 @@ module.exports = function robook ({ command = 'watch', dir = '.', port = 4000 },
   debug(`Processing ${command} ${dir}`);
 
   if (command === 'watch') {
-    server(dir, port, (err) => {
+    server(dir, port, (err, app, channel) => {
       if (err) return cb(err);
       let dw = new DirectoryWatchingIngester({ preIngest: true })
         , reportError = makeErrorReporter()
         , hasInit = false
+        , nav = []
+        , index = null
       ;
       dw.on('error', reportError);
       dw.on('init', () => {
         hasInit = true;
         // XXX: at this point we have all the search and nav ready, send it to SEE
+        channel.send({ nav, index });
       });
       dw.on('file', (source) => {
         if (!hasInit) return;
@@ -29,6 +32,7 @@ module.exports = function robook ({ command = 'watch', dir = '.', port = 4000 },
           //  - search index
           //  - add to nav
           //    - for both of the above we need to process the MD in full anyway, so just use that
+          //    - it is worth noting that the nav should largely be in an index file?
           //  - tell SSE but only if hasInit
         }
       });
